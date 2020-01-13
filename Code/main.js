c43a7f09e7c8a427d6b084dcee69a8c68294b847
cristian98st <44675214+cristian98st@users.noreply.github.com>
2020-01-13 18:02:18 +0200

    Pages

---
+++ Code/main.js
@@ -0,0 +1,31 @@
+window.onhashchange = function(){
+    render(window.location.hash)
+}
+
+function render(hashKey){
+    let pages = document.querySelectorAll(".page");
+    
+    // disable all
+    for(let i = 0; i< pages.length; ++i) {
+        pages[i].style.display = 'none';
+    }
+
+    // enable the right one
+    switch(hashKey){
+        case "":
+            pages[0].style.display = 'block';
+            break;
+        case "#acquaintances":
+            pages[0].style.display = 'block';
+            break;
+        case "#friends":
+            pages[1].style.display = 'block';
+            break;
+        case "#connect":
+            pages[2].style.display = 'block';
+            break;
+        default:
+            pages[0].style.display = 'block';
+    }
+
+}
