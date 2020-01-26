fd6c211f3de0959f1f13851265c93da065a04ab2
cristian98st <44675214+cristian98st@users.noreply.github.com>
2020-01-26 18:27:56 +0200

    SPA attempt 1

--- Code/server.js
+++ Code/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 app.use(express.static(__dirname + '/views/public'));
 
 // set the view engine to ejs
@@ -8,31 +9,29 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 app.get('/', function(req, res) {
-    res.render('partials/index',{
-        page: 'friends'
-    });
+    res.sendFile(path.join(__dirname+'/views/partials/index.html'));
 });
-app.get('/links', function(req, res) {
-    res.render('partials/index',{
-        page: 'links'
-    });
-});
-app.get('/acquaintances', function(req, res) {
-    res.render('partials/index',{
-        page: 'acquaintances'
-    });
-});
-app.get('/friends', function(req, res) {
-    res.render('partials/index',{
-        page: 'friends'
-    });
-});
+// app.get('/links', function(req, res) {
+//     res.render('partials/index',{
+//         page: 'links'
+//     });
+// });
+// app.get('/acquaintances', function(req, res) {
+//     res.render('partials/index',{
+//         page: 'acquaintances'
+//     });
+// });
+// app.get('/friends', function(req, res) {
+//     res.render('partials/index',{
+//         page: 'friends'
+//     });
+// });
 
-app.get('/signup', function(req, res){
-    res.render('partials/index', {
-        page: 'signup'
-    });
-});
+// app.get('/signup', function(req, res){
+//     res.render('partials/index', {
+//         page: 'signup'
+//     });
+// });
 
 app.listen(3000);
 console.log('listening to 3000...');
